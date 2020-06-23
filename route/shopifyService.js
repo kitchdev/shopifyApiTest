d61e6986a6b2a09e6fca472d4b110aa4f79ecfb1
Mathew <gdsm.kitching@gmail.com>
2020-06-23 16:05:37 -0400

    config functional + console.time

--- route/shopifyService.js
+++ route/shopifyService.js
@@ -1,6 +1,15 @@
 const Shopify = require("shopify-api-node");
 const sleep = require('util').promisify(setTimeout)
-const config = require('config')
+const Config = require('config')
+
+const config = {
+  shopName: Config.get('shopifyApi.shopName'),
+  accessToken: Config.get('shopifyApi.accessToken'),
+  apiVersion: Config.get('shopifyApi.apiVersion'),
+  autoLimit: Config.get('shopifyApi.autoLimit'),
+  timeout: Config.get('shopifyApi.timeout'),
+}
+
 
 exports.listCustomersPaginate = async (req, res) => {
   req.setTimeout(600000)
@@ -20,11 +29,12 @@ exports.listCustomersPaginate = async (req, res) => {
   let page = 0
   let callLimit = 40
   let percentageComplete
-  const query = `updated_at:>"2019-07-02T18:06:31-05:00"`
-  // const query = `orders_count:>=0 order_date:>"2019-07-02T18:06:31-05:00"`
+  // const query = `updated_at:>"2019-07-02T18:06:31-05:00"`
+  const query = `orders_count:>=0 order_date:>"2019-07-02T18:06:31-05:00"`
   try {
     await (async () => {
       let params = { limit, fields, query }
+      console.time('import-customer')
       do {
         customers = await shopify.customer.search(params)
         customersBatchs.push({ page: page + 1 }, ...customers)
@@ -34,12 +44,13 @@ exports.listCustomersPaginate = async (req, res) => {
         params = customers.nextPageParameters
         console.log({ percentageComplete })
         if (callLimit === 0) {
-          console.log('sleeping for 10 seconds')
-          await sleep(12000)
+          // console.log('sleeping for 10 seconds')
+          // await sleep(12000)
           callLimit = 40
         }
       } while (params !== undefined)
     })()
+    console.timeEnd('import-customer')
     console.log('Process complete')
     res.status(200).send(customersBatchs)
   } catch (err) {
