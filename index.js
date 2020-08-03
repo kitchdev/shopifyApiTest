fa0e2ce63abb55ae7cc2afa3fb5d30426b61fdea
Mathew <gdsm.kitching@gmail.com>
2020-08-03 10:44:41 -0400

    get orders route added

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const { listCustomersPaginate } = require('./route/shopifyService')
+const { 
+  listCustomersPaginate, 
+  listAllOrdersPaginate,
+  listCancelledOrders,
+  listFufilledOrders
+} = require('./route/shopifyService')
+
 
 app.get('/listCustomers', listCustomersPaginate)
+
+app.get('/listAllOrders', listAllOrdersPaginate)
+
+app.get('listCancelledOrders', listCancelledOrders)
+
+app.get('/listFufilledOrders', listFufilledOrders)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- route/shopifyService.js
+++ route/shopifyService.js
@@ -14,6 +14,7 @@ const config = {
 exports.listCustomersPaginate = async (req, res) => {
   req.setTimeout(600000)
   const shopify = new Shopify(config)
+  console.log(shopify)
   const fields = [
     'id',
     'updated_at',
@@ -36,7 +37,7 @@ exports.listCustomersPaginate = async (req, res) => {
       let params = { limit, fields, query }
       console.time('import-customer')
       do {
-        customers = await shopify.customer.search(params)
+        const customers = await shopify.customer.search(params)
         customersBatchs.push({ page: page + 1 }, ...customers)
         page++
         callLimit--
@@ -56,4 +57,61 @@ exports.listCustomersPaginate = async (req, res) => {
   } catch (err) {
     res.status(400).send(err)
   }
+}
+
+exports.listAllOrdersPaginate = async (req, res) => {
+  // req.setTimeout(600000)
+  const shopify = new Shopify(config)
+  console.log(shopify)
+  try {
+    const limit = 250
+    const orderCount = await shopify.order.count()
+    console.log(orderCount)
+    const totalPages = Math.ceil(orderCount / limit)
+    const updated_at_min = "2019-07-02T18:06:31-05:00"
+    let orderBatchs = []
+    let page = 0
+    let callLimit = 40
+    let percentageComplete
+    // const query = `updated_at:>"2019-07-02T18:06:31-05:00"`
+    // const query = `orders_count:>=0 order_date:>"2019-07-02T18:06:31-05:00"`
+  
+    await (async () => {
+      const status = { status: 'any' }
+      let params = { limit, created_at_min: '2018-04-25T16:15:47-04:00'}
+      console.time('list-orders')
+      do {
+        const orders = await shopify.order.list(params, status)
+        console.log(orders)
+        orderBatchs.push({ page: page + 1 }, ...orders)
+        page++
+        callLimit--
+        percentageComplete = Math.floor((page / totalPages) * 100)
+        params = orders.nextPageParameters
+        console.log({ percentageComplete })
+        if (callLimit === 0) {
+          // console.log('sleeping for 10 seconds')
+          // await sleep(12000)
+          callLimit = 40
+        }
+      } while (params !== undefined)
+    })()
+    console.timeEnd('list-orders')
+    console.log('Process complete')
+    res.status(200).send(orderBatchs)
+  } catch (err) {
+    console.error('error =>', err)
+    res.status(400).send(err)
+  }
+}
+
+
+exports.listCancelledOrders = async (req, res) => {
+  req.setTimeout(600000)
+  const shopify = new Shopify(config)
+}
+
+exports.listFufilledOrders = async (req, res) => {
+  req.setTimeout(600000)
+  const shopify = new Shopify(config)
 }
